021f306f4b72b60a43fe6e82b52e0fdc08466944
Jang CY <wkdcjfdud13@gmail.com>
2024-03-06 16:56:33 +0900

    a

---
+++ lecture/example.js
@@ -0,0 +1,11 @@
+/*
+ㅁㄴㅇㄹ
+asdf
+1234
+*/
+
+var name = prompt("이름을 입력해 주세요."); // scanf랑 비슷하다?
+
+var msg = "당신의 이름은 "+name+"입니다. "+name+"님의 이름은 "+name.length+"글자 입니다.";
+
+console.log(msg);
---
+++ lecture/lecture12.js
@@ -0,0 +1,3 @@
+2 * 3 > 4 + 5 && 6 / 3 == 2 || !false
+
+height >= 180 && gender == "male" || height >= 165 && gender == "female"
