0bcbac027fb70ad325668fe94781c403b1bd311c
Jang CY <wkdcjfdud13@gmail.com>
2024-03-06 14:42:08 +0900

    a

--- lecture/lecture09.js
+++ lecture/lecture09.js
@@ -3,5 +3,7 @@ var b = 3;
 
 console.log(a + b, a - b, a * b, a / b, a % b);
 
-console.log(--a);
-console.log(a);
+
+
+console.log( Math.pow(a, b), Math.floor(a, b), Math.round(Math.sqrt(5)));
+console.log( Math.random() );
